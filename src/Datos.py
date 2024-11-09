229ce7468c1e7597d29f1bc281e3239f9a50635f
Sara Lucia Jara <santiago.londonos@upb.edu.com>
2024-11-09 02:23:44 -0500

    Final commit

--- src/Datos.py
+++ src/Datos.py
@@ -1,6 +1,7 @@
 import csv 
 import matplotlib.pyplot as plt 
 import os 
+import numpy as np
 
 # Funciones archivos de texto(.txt)
 
@@ -89,11 +90,18 @@ def cal_est(archivo_csv):
             #Encontrar columna 
             if columna in encabezados: 
                 print("Columna encontrada")
+                indice_columna = encabezados.index(columna)
+                for fila in lector: 
+                    try: 
+                        valor = float(fila[indice_columna])
+                        info. append(valor)
+                    except ValueError:
+                        print("El valor de la fila no es un número")
             else:  
                 print("Ingrese una columna válida") 
+                return
 
             #Calcular estadísticas
-
             if info: 
                 numero_datos = len(info)
                 promedio = sum(info)/numero_datos
@@ -110,19 +118,110 @@ def cal_est(archivo_csv):
     except FileNotFoundError:
         print("Archivo no encontrado. Verifique e intente nuevamente")
 
+#Función grafica
+def grafica(archivo_csv):
+    try: 
+     columna = input("Ingrese la columna que desea graficar: ")
+     info = []
+     with open(archivo_csv, 'r') as csvfile: 
+            lector = csv.reader(csvfile)
+            encabezados = next(lector)
+            if columna in encabezados: 
+                print("Columna encontrada")
+                indice_columna = encabezados.index(columna)
+                for fila in lector: 
+                    try: 
+                        valor = float(fila[indice_columna])
+                        info.append(valor)
+                    except ValueError:
+                        print (f"El valor '{fila[indice_columna]}' de la fila no es un número y será ignorado")
+                        columna_x = input("Ingrese el nombre para el eje x:")
+                        columna_y = input("Ingrese el nombre del eje y: ")         
+                        if info: 
+                          plt.plot(info)
+                          plt.title("Gráfica de la columna '{columna}' ")
+                          plt.xlabel(columna_x)
+                          plt.ylabel(columna_y)
+                          plt. show()
+                        else:
+                            print ("No hay datos numéricos para graficar la columna ")
+            else: 
+                print("Columna no encontrada. Verifique e intente nuevamente")
+    except FileNotFoundError: 
+     print("Archivo no encontrado. Verifique e intente nuevamente")
 
+#Función submenú 
+def men_arch_csv():
+    while True: 
+        opcion_csv = input("Seleccione el número de la opción que desea realizar. \n1. Mostrar 15 primeras filas \n2. Calcular estadísiticas \n3. Graficar columna  \n4. Menú principal").strip()
+        archivo_csv = input("Ingrese la rutal del archivo").strip('"').strip("'")
+        if opcion_csv == 1:
+            leer_filas(archivo_csv)
+        elif opcion_csv == 2:
+            cal_est(archivo_csv)
+        elif opcion_csv == 3:
+            grafica(archivo_csv)
+        elif opcion_csv == 4:
+            print("Regresando al menú principal")
+            break
+        else: 
+            print("Opción inválida. Seleccione una correcta")
 
+#Funciones archivos
+
+#Función listar archivos 
+def listar():
+    ruta_actual = os.getcwd()
+    print(f"Archivos de la rutal actual: {ruta_actual}")
+    archivos = os.listdir(ruta_actual)
+    for archivo in archivos:
+        if os.path.isfile(os.path.join(ruta_actual, archivo)):
+            print(archivo)
+
+#Función ingresar ruta donde listar archivos 
+def ruta_listar():
+    ruta = input("Ingrese la ruta que desea listar: "). strip('"').strip("'")
+    if os.path.isdir (ruta):
+        print (f"Archivos en el directorio ({ruta})")
+        for archivo in os.listdir(ruta):
+            if os.path.isfile(os.path.join(ruta,archivo)):
+                print(archivo)
+    else: 
+        print("La ruta ingresada es inválida o no existe")
+
+#Función submenú 
+def men_arch():
+    while True: 
+        opcion_arch = input("Seleccione el número de la opción que desea realizar. \n1. Listar archivos en la ruta actual  \n2. Ingresar una ruta donde listar archivos \n3. Menú principal")
+        if opcion_arch == 1:
+            listar()
+        elif opcion_arch == 2:
+            ruta_listar()
+        elif opcion_arch == 3:
+            print("Regresando al menú principal")
+            break
+        else:
+            print("Opción inválida. Seleccione una correcta")
             
-
-                
 
 
 #Función menú principal 
 def menu():
     while True: 
         opcion = int(input("¿Qué desea realizar el día de hoy? \n1. Listar archivos \n2. Procesar archivo (.txt) \n3. Procesar archivo (.csv) \n4. Salir"))
-        if opcion == "2": 
+        if opcion == 1: 
+            men_arch()
+        elif opcion == 2: 
             menu_arch_txt()
+        elif opcion == 3:
+            men_arch_csv()
+        elif opcion == 4:
+            print("Saliendo del programa")
+            break 
+    else:
+        print("Ingrese una opción correcta")
+
+            
 
 
 
